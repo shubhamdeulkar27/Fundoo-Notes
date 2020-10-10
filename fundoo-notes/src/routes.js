346946b9701276d16d480ecd8e9241623ba4844a
shubham <shubhamdeulkar27@gmail.com>
2020-10-10 08:16:24 +0530

    Added: Implemented Route Guard

--- fundoo-notes/src/routes.js
+++ fundoo-notes/src/routes.js
@@ -5,24 +5,40 @@ import ArchivedNotes from "./components/ArchivedNotes.vue";
 import TrashedNotes from "./components/TrashedNotes.vue";
 import AddNote from "./components/AddNote.vue";
 import DisplayNotes from "./components/DisplayNotes.vue";
+function guardRoute(to, from, next) {
+  var isAuthenticated = false;
+  if (localStorage.getItem("fundoo-token")) {
+    isAuthenticated = true;
+  } else {
+    isAuthenticated = false;
+  }
+  if (isAuthenticated) {
+    next();
+  } else {
+    next("/login");
+  }
+}
+
 export default [
   { path: "/", component: Login },
   { path: "/login", component: Login },
   {
     path: "/dashboard",
     component: Dashboard,
+    beforeEnter: guardRoute,
     children: [
-      { path: "/", component: Notes },
+      { path: "/", component: Notes, beforeEnter: guardRoute },
       {
         path: "/notes",
         component: Notes,
+        beforeEnter: guardRoute,
         children: [
           { path: "/add", component: AddNote },
           { path: "/display", component: DisplayNotes }
         ]
       },
-      { path: "/archived", component: ArchivedNotes },
-      { path: "/trashed", component: TrashedNotes }
+      { path: "/archived", component: ArchivedNotes, beforeEnter: guardRoute },
+      { path: "/trashed", component: TrashedNotes, beforeEnter: guardRoute }
     ]
   }
 ];
